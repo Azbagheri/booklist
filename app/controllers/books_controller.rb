688140967a7d32243bfad1fa5f22ba0caf1a8784
Azbagheri <azadeh.bagheri7@gmail.com>
2017-04-26 15:08:11 -0400

    The list of books is shown in json format

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,6 +7,13 @@ class BooksController < ApplicationController
       format.html
       format.text
       format.csv {render plain: Book.generate_csv(@books)}
+      format.json do
+        render json: @books.map do |book|
+          {title: book.title,
+          author: book.author,
+          already_read: book.already_read}
+        end
+      end
     end
   end
 
